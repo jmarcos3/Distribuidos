a26d31f6311c8256a9393df587fa620317691e69
jmarcos3 <jmarcosnsf@gmail.com>
2024-07-04 16:08:03 -0300

    Fim

--- cliente.py
+++ cliente.py
@@ -6,15 +6,14 @@ from watchdog.events import FileSystemEventHandler
 
 # Lista de IPs/portas dos outros nós na rede
 PEERS = [
-    'http://127.0.0.1:8085',  # URL do servidor XML-RPC
-    # Adicione mais nós conforme necessário
+    'http://192.168.0.177:8084', # IP 1
 ]
 
 class Watcher:
     def __init__(self):
         self.DIRECTORY_TO_WATCH = os.getcwd()  # Diretório atual
         self.observer = Observer()
-        self.processed_files = set()  # Conjunto para arquivos processados
+        self.processed_files = set()  # Conjunto de arquivos processados
 
     def run(self):
         event_handler = Handler(self.processed_files)
@@ -22,7 +21,7 @@ class Watcher:
         self.observer.start()
         try:
             while True:
-                time.sleep(1)  # Reduzido o tempo de espera para 1 segundo para melhor responsividade
+                time.sleep(1)  
         except KeyboardInterrupt:
             self.observer.stop()
         self.observer.join()
@@ -33,40 +32,48 @@ class Handler(FileSystemEventHandler):
         self.clients = [ServerProxy(peer) for peer in PEERS]
         self.processed_files = processed_files
 
-    def propagate_change(self, file_name, content, action):
+    def propagate_change(self, file_name, content, action):  # Função que envia o pacote aos endereços conectados
         for client in self.clients:
             try:
                 client.handle_package(file_name, content, action)
             except Exception as e:
                 print(f"Erro ao comunicar com peer: {e}")
 
-    def on_created(self, event):
+    def on_created(self, event):  # Função de criação
         if not event.is_directory:
             file_name = event.src_path
-            if file_name not in self.processed_files:
-                print(f"{file_name} foi criado.")
-                with open(file_name, 'rb') as file:
-                    content = file.read()
-                self.propagate_change(file_name, content, "Created")
-                self.processed_files.add(file_name)  # Adiciona o arquivo ao conjunto de arquivos processados
+            base_name = os.path.basename(file_name)
+            
+            if not base_name.startswith("1"): 
+                print(f"{base_name} foi criado localmente.")
+                with open(file_name, 'rb') as file: # Abro o arquivo que foi criado
+                    content = file.read() # leio o conteudo dentro do arquvo e guardo em uma variavel
+                self.propagate_change(base_name, content, "Created") # e mando todo o conteudo para o servidor
+                self.processed_files.add(base_name) # adiciono o nome de arquivos na lista de arquivos a serem trackeados
 
     def on_modified(self, event):
         if not event.is_directory:
             file_name = event.src_path
-            if file_name not in self.processed_files:
-                print(f"{file_name} foi modificado.")
+            base_name = os.path.basename(file_name)
+            
+            ''' Esse If de checagem que occore em todos os métodos é para garantir que só o autor que possui o nome do arquivo original consiga modificar e deletar para todos
+            já que quando o servidor cria um arquivo enviado por outra maquina ele adiciona esse "1" na frente, existem outras soluções mas foi a que a gnt pensou de forma mais rápida
+            para uma solução inicial.'''
+            if not base_name.startswith("1"):  
+                print(f"{base_name} foi modificado localmente.")
                 with open(file_name, 'rb') as file:
                     content = file.read()
-                self.propagate_change(file_name, content, "Modified")
-                self.processed_files.add(file_name)  # Adiciona o arquivo ao conjunto de arquivos processados
+                self.propagate_change(base_name, content, "Modified") # manda as modificações para o servidor através desas função
 
     def on_deleted(self, event):
         if not event.is_directory:
             file_name = event.src_path
-            if file_name in self.processed_files:
-                print(f"{file_name} foi deletado.")
-                self.propagate_change(file_name, b'', "Deleted")
-                self.processed_files.remove(file_name)  # Remove o arquivo do conjunto de arquivos processados
+            base_name = os.path.basename(file_name)
+            
+            if not base_name.startswith("1"):  
+                print(f"{base_name} foi deletado localmente.")
+                self.propagate_change(base_name, b'', "Deleted") # manda pro servidor
+                self.processed_files.discard(base_name) # remove da lista de arquivos a serem "trackeados"
 
 if __name__ == '__main__':
     print("Monitoramento Online em:", os.getcwd()) 
--- servidor.py
+++ servidor.py
@@ -1,7 +1,11 @@
 from xmlrpc.server import SimpleXMLRPCServer
 import os
+from xmlrpc.client import Binary
 
 class FileSyncServer:
+    def __init__(self):
+        self.base_directory = os.getcwd()
+
     def handle_package(self, file_name, content, action):
         try:
             if action == "Created":
@@ -16,30 +20,33 @@ class FileSyncServer:
             return False
 
     def create_file(self, file_name, content):
-        with open(file_name, 'wb') as f:  # Abre o arquivo no modo binário 'wb'
-            f.write(content)
-        print(f"{file_name} foi criado com sucesso.")
+        full_file_name = os.path.join(self.base_directory,"1"+file_name) # ele define o nome do arquivo adicionando o 1 na frente, que significa que foi criado pelo servidor
+        with open(full_file_name, 'wb') as f:
+            f.write(content.data if isinstance(content, Binary) else content)
+        print(f"{full_file_name} foi criado com sucesso.")
 
     def modify_file(self, file_name, content):
-        if os.path.exists(file_name):
-            with open(file_name, 'wb') as f:  # Abre o arquivo no modo binário 'wb'
-                f.write(content)
-            print(f"{file_name} foi modificado com sucesso.")
+        full_file_name = os.path.join(self.base_directory,"1"+file_name)
+        if os.path.exists(full_file_name):
+            with open(full_file_name, 'wb') as f:
+                f.write(content.data if isinstance(content, Binary) else content)
+            print(f"{full_file_name} foi modificado com sucesso.")
         else:
             print(f"Erro ao modificar {file_name}: Arquivo não encontrado.")
 
     def delete_file(self, file_name):
-        if os.path.exists(file_name):
-            os.remove(file_name)
-            print(f"{file_name} foi deletado com sucesso.")
+        full_file_name = os.path.join(self.base_directory,"1"+file_name)
+        if os.path.exists(full_file_name):
+            os.remove(full_file_name)
+            print(f"{full_file_name} foi deletado com sucesso.")
         else:
             print(f"Erro ao deletar {file_name}: Arquivo não encontrado.")
 
 def run_server(port):
-    server = SimpleXMLRPCServer(('localhost', port))
+    server = SimpleXMLRPCServer(('localhost', port)) # Mudar o local host para o endereço da máquina
     server.register_instance(FileSyncServer())
     print(f"Servidor RPC está rodando na porta {port}...")
     server.serve_forever()
 
 if __name__ == "__main__":
-    run_server(8085)  # Modifique a porta conforme necessário
+    run_server(8084)  # Modifique a porta conforme necessário
